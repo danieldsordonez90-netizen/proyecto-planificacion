1c41892eb4579eff9e1de16f45c1ca02f66b8c34
Alesss-D0706 <Idiazvillanueva321@gmail.com>
2026-07-17 22:44:15 -0600

    Se agregaron más hechos, aún incompleto

--- data-model/db.pl
+++ data-model/db.pl
@@ -4,23 +4,65 @@ estudiante('20221000760', 'Carlos Miguel Hernandez', 'carlos.martinez@unah.hn', 
 estudiante('20221000761', 'Maria Fernanda Antunez', 'maria.macarena@unah.hn', 'ingeniería en sistemas', 69).
 estudiante('20221000762', 'Jose Antonio Salazar', 'jose@unah.hn', 'ingeniería en sistemas', 85).
 estudiante('20211000123', 'Marta Antonieta Carías','mcarias@unah.hn','ingenieria en sistemas', 78).
+estudiante('20231000112', 'Sofía Alejandra Martínez', 'sofia.martinez@unah.hn', 'ingeniería en sistemas', 88).
+estudiante('20231000445', 'Diego Fernando Ramos', 'diego.ramos@unah.hn', 'ingeniería en sistemas', 74).
+estudiante('20221000890', 'Lucía Gabriela Varela', 'lucia.varela@unah.hn', 'ingeniería en sistemas', 92).
+estudiante('20241000234', 'Andrés Mauricio Castro', 'andres.castro@unah.hn', 'ingeniería en sistemas', 81).
+estudiante('20211000956', 'Elena Beatriz Flores', 'elena.flores@unah.hn', 'ingeniería en sistemas', 76).
+estudiante('20231000501', 'Juan Pablo Zelaya', 'jpzelaya@unah.hn', 'ingeniería en sistemas', 78).
+estudiante('20231000502', 'Valeria Nicolle Paz', 'vnicolle@unah.hn', 'ingeniería en sistemas', 92).
+estudiante('20221000503', 'Gabriel Alejandro Mejía', 'gabriel.mejia@unah.hn', 'ingeniería en sistemas', 62).
+estudiante('20241000504', 'Camila Alejandra Sosa', 'camila.sosa@unah.hn', 'ingeniería en sistemas', 84).
+estudiante('20211000505', 'Ricardo David Moncada', 'ricardo.moncada@unah.hn', 'ingeniería en sistemas', 70).
+estudiante('20231000506', 'Daniela María Argueta', 'dargueta@unah.hn', 'ingeniería en sistemas', 75).
+estudiante('20221000507', 'Eduardo Josué Bonilla', 'ejbonilla@unah.hn', 'ingeniería en sistemas', 68).
+estudiante('20241000508', 'Sara Victoria Lanza', 'sara.lanza@unah.hn', 'ingeniería en sistemas', 98).
+estudiante('20231000509', 'Fernando José Cruz', 'fcruz@unah.hn', 'ingeniería en sistemas', 81).
+estudiante('20221000510', 'Andrea Michelle Madrid', 'amichelle@unah.hn', 'ingeniería en sistemas', 73).
+estudiante('20231000511', 'Luis Armando Castro', 'lacastro@unah.hn', 'ingeniería en sistemas', 64).
+estudiante('20211000512', 'Paola Alejandra Reyes', 'pareyes@unah.hn', 'ingeniería en sistemas', 85).
+estudiante('20241000513', 'Marco Antonio Pineda', 'mapineda@unah.hn', 'ingeniería en sistemas', 77).
+estudiante('20231000514', 'Gabriela Monserrat Videa', 'gmvidea@unah.hn', 'ingeniería en sistemas', 82).
+estudiante('20221000515', 'Carlos Alberto Fúnez', 'cafunez@unah.hn', 'ingeniería en sistemas', 69).
+estudiante('20231000516', 'Sofía Irene Ordóñez', 'sordonez@unah.hn', 'ingeniería en sistemas', 95).
+estudiante('20241000517', 'Javier Ignacio Matamoros', 'jmatamoros@unah.hn', 'ingeniería en sistemas', 71).
+estudiante('20221000518', 'Beatriz Elena Valladares', 'bvalladares@unah.hn', 'ingeniería en sistemas', 80).
+estudiante('20231000519', 'Héctor Manuel Turcios', 'hturcios@unah.hn', 'ingeniería en sistemas', 66).
 estudiante('', '', '', '',).
 
 
-profesor('0001', 'Miguel Angel Rodas', 'miguel.sauceda@unah.hn').
-profesor('0002', 'Juan Andres Ceballos', 'juan.rodriguez@unah.hn').
-profesor('0003', 'Mario José Gutierrez', 'mario.ramirez@unah.hn').
+profesor('0001', 'Miguel Angel Rodas', 'miguel.rodas@unah.hn').
+profesor('0002', 'Juan Andres Ceballos', 'juan.ceballos@unah.hn').
+profesor('0003', 'Mario José Gutierrez', 'mario.gutierrez@unah.hn').
+profesor('0004', 'Ana Lucía Zelaya', 'ana.zelaya@unah.hn').
+profesor('0005', 'Carlos Roberto Martínez', 'carlos.martinez@unah.hn').
+profesor('0006', 'Elena María Castro', 'elena.castro@unah.hn').
+profesor('0007', 'Ricardo Javier Mejía', 'ricardo.mejia@unah.hn').
+profesor('0008', 'Sofía Alejandra Paz', 'sofia.paz@unah.hn').
+profesor('0009', 'Diego Armando Flores', 'diego.flores@unah.hn').
+profesor('0010', 'Valeria Gabriela Sosa', 'valeria.sosa@unah.hn').
+profesor('0011', 'Eduardo Josué Varela', 'eduardo.varela@unah.hn').
+profesor('0012', 'Daniela Irene Argueta', 'daniela.argueta@unah.hn').
+profesor('0013', 'Marco Antonio Bonilla', 'marco.bonilla@unah.hn').
+profesor('0014', 'Sara Victoria Lanza', 'sara.lanza@unah.hn').
 
-materia('isc101', 'paradigmas-de-programacion', 4).
-materia('isc102', 'bases-de-datos', 5).
-materia('isc103', 'inteligencia-artificial', 4).
-
+materia('isc101', ' ','Introduccion a la ingeniería en sistemas Computacionales', 4).
+materia('isc102', ' ','Programación Estructurada', 4).
+materia('isc103', ' ','Programación Orientada a Objetos', 5).
+materia('isc204', ' ','Paradigmas de Programación', 4).
+materia('isc211', ' ','Estructuras de Datos', 4).
+materia('isc321', ' ','Fundamentos Bases de Datos', 5).
+materia('isc331', ' ','Redes de Datos I', 4).
+materia('fs100', ' ','Fisica General I', 5).
 
 seccion('isc101', '0700', '0001').
 seccion('isc102', '0700', '0001').
 seccion('isc102', '0800', '0002').
 seccion('isc103', '0800', '0003').
 seccion('isc103', '0900', '0001').
+
+laboratorio('0701', 'fs200', 'E1', 'Laboratorio de Física').
+
 
 matriculado('20221000759', 'isc101').
 matriculado('20221000759', 'isc103').
@@ -48,3 +90,5 @@ lista_alumnos_de_profesor(NombreProfesor, ListaAlumnos) :-
 
 lista_alumnos_seccion(NombreProfesor, CodigoMateria, ListaAlumnos) :-
     findall(NombreEstudiante, alumnos_en_seccion_de_profesor(NombreProfesor, CodigoMateria, NombreEstudiante), ListaAlumnos).
+
+
